6cca67b4d25f796843215d9e0010ce1df7177406
ejatkin <ejatkin@gmail.com>
2017-01-26 15:31:05 +0000

    Added grid to board

--- lib/board.rb
+++ lib/board.rb
@@ -1,6 +1,14 @@
 class Board
 
-  
+  attr_reader :grid
+
+  def initialize
+    @grid = {"A1" => " ", "A2" => " ", "A3" => " ", "B1" => " ",
+            "B2" => " ", "B3" => " ", "C1" => " ", "C2" => " ", "C3" => " "}
+
+  end
+
+
 
 
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -2,5 +2,9 @@ require 'board'
 
 describe Board do
 
-  
+  it "can have a grid of 9 places" do
+    board = Board.new
+    expect(board.grid.count).to eq 9
+  end
+
 end
