b546c1515d68fcd9221dd034f27e7fce25c4d4a6
ejatkin <ejatkin@gmail.com>
2017-01-26 15:07:58 +0000

    Minor refactoring of cell tests

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -2,13 +2,13 @@ require 'cell'
 
 describe Cell do
 
+  let(:cell){Cell.new}
+
   it "should have an initial value of untaken/empty" do
-    cell = Cell.new
     expect(cell.value).to eq " "
   end
 
   it "knows when it has been filled with an X or O" do
-    cell = Cell.new
     expect(cell.taken?).to eq true
   end
 
